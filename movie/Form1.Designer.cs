9a64ed853215aa296eb9583ab4cc71a7132aa098
AlinaChermashentseva <62728584+AlinaChermashentseva@users.noreply.github.com>
2020-06-04 01:59:45 +0300

    1

---
+++ movie/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Task_6__Animation_
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.MercuryTimer = new System.Windows.Forms.Timer(this.components);
+            this.AnimationCheckBox = new System.Windows.Forms.CheckBox();
+            this.EarthTimer = new System.Windows.Forms.Timer(this.components);
+            this.MarsTimer = new System.Windows.Forms.Timer(this.components);
+            this.JupiterTimer = new System.Windows.Forms.Timer(this.components);
+            this.SaturnTimer = new System.Windows.Forms.Timer(this.components);
+            this.UranusTimer = new System.Windows.Forms.Timer(this.components);
+            this.NeptuneTimer = new System.Windows.Forms.Timer(this.components);
+            this.PlutoTimer = new System.Windows.Forms.Timer(this.components);
+            this.VenusTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPictureBox.BackColor = System.Drawing.Color.Black;
+            this.MainPictureBox.Location = new System.Drawing.Point(9, 67);
+            this.MainPictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(986, 564);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // MercuryTimer
+            // 
+            this.MercuryTimer.Interval = 1;
+            this.MercuryTimer.Tick += new System.EventHandler(this.MercuryTimer_Tick);
+            // 
+            // AnimationCheckBox
+            // 
+            this.AnimationCheckBox.AutoSize = true;
+            this.AnimationCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AnimationCheckBox.Location = new System.Drawing.Point(455, 23);
+            this.AnimationCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.AnimationCheckBox.Name = "AnimationCheckBox";
+            this.AnimationCheckBox.Size = new System.Drawing.Size(98, 28);
+            this.AnimationCheckBox.TabIndex = 0;
+            this.AnimationCheckBox.Text = "Animate";
+            this.AnimationCheckBox.UseVisualStyleBackColor = true;
+            this.AnimationCheckBox.CheckedChanged += new System.EventHandler(this.AnimationCheckBox_CheckedChanged);
+            // 
+            // EarthTimer
+            // 
+            this.EarthTimer.Interval = 20;
+            this.EarthTimer.Tick += new System.EventHandler(this.EarthTimer_Tick);
+            // 
+            // MarsTimer
+            // 
+            this.MarsTimer.Interval = 35;
+            this.MarsTimer.Tick += new System.EventHandler(this.MarsTimer_Tick);
+            // 
+            // JupiterTimer
+            // 
+            this.JupiterTimer.Interval = 60;
+            this.JupiterTimer.Tick += new System.EventHandler(this.JupiterTimer_Tick);
+            // 
+            // SaturnTimer
+            // 
+            this.SaturnTimer.Interval = 150;
+            this.SaturnTimer.Tick += new System.EventHandler(this.SaturnTimer_Tick);
+            // 
+            // UranusTimer
+            // 
+            this.UranusTimer.Interval = 420;
+            this.UranusTimer.Tick += new System.EventHandler(this.UranusTimer_Tick);
+            // 
+            // NeptuneTimer
+            // 
+            this.NeptuneTimer.Interval = 825;
+            this.NeptuneTimer.Tick += new System.EventHandler(this.NeptuneTimer_Tick);
+            // 
+            // PlutoTimer
+            // 
+            this.PlutoTimer.Interval = 1240;
+            this.PlutoTimer.Tick += new System.EventHandler(this.PlutoTimer_Tick);
+            // 
+            // VenusTimer
+            // 
+            this.VenusTimer.Interval = 18;
+            this.VenusTimer.Tick += new System.EventHandler(this.VenusTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 642);
+            this.Controls.Add(this.AnimationCheckBox);
+            this.Controls.Add(this.MainPictureBox);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Animation";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.CheckBox AnimationCheckBox;
+        private System.Windows.Forms.Timer MercuryTimer;
+        private System.Windows.Forms.Timer EarthTimer;
+        private System.Windows.Forms.Timer MarsTimer;
+        private System.Windows.Forms.Timer JupiterTimer;
+        private System.Windows.Forms.Timer SaturnTimer;
+        private System.Windows.Forms.Timer UranusTimer;
+        private System.Windows.Forms.Timer NeptuneTimer;
+        private System.Windows.Forms.Timer PlutoTimer;
+        private System.Windows.Forms.Timer VenusTimer;
+    }
+}
+
---
+++ movie/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task_6__Animation_
+{
+  public partial class MainForm : Form
+    {
+        Graphics g;
+        Random rand;
+        Bitmap bitmp;
+        int[] coordinateX = new int[986]; // смещение по Х
+        int[] coordinateY = new int[564]; // смещение по Y
+
+        Planet Mercury = new Planet(60, 493-7, 282 - 7);
+        Planet Venus = new Planet(90, 493 - 10, 282 - 10);
+        Planet Earth = new Planet(120, 493 - 12, 282 - 12);
+        Planet Mars = new Planet(147, 493 - 7, 282 - 7);
+        Planet Jupiter = new Planet(200, 493 - 22, 282 - 22);
+        Planet Saturn = new Planet(250, 493 - 20, 282 - 20);
+        Planet Uranus = new Planet(295, 493 - 17, 282 - 17);
+        Planet Neptune = new Planet(350, 493 - 18, 282 - 18);
+        Planet Pluto = new Planet(390, 493 - 6, 282 - 6);
+
+        public MainForm()
+        {
+            rand = new Random((int)DateTime.Now.Ticks);
+            InitializeComponent();
+            bitmp = new Bitmap(MainPictureBox.Width, MainPictureBox.Height);
+            g = Graphics.FromImage(bitmp);
+
+            for (int i = 0; i < 564; i++)
+            {
+                coordinateX[i] = rand.Next(986);
+                coordinateY[i] = rand.Next(564);
+            }
+        }
+
+        private void DrawStars(Bitmap bitmp, int coordinateX, int coordinateY)
+        {
+            g.FillEllipse(Brushes.White, MainPictureBox.Width - coordinateX, MainPictureBox.Height - coordinateY, 2, 2);           
+            MainPictureBox.Image = bitmp;
+        }
+
+        private void DrawPlanet(Bitmap bitmp)
+        {
+            g.FillEllipse(Brushes.Yellow, (MainPictureBox.Width/2)-40, (MainPictureBox.Height / 2) - 40, 80, 80);
+            g.FillEllipse(Brushes.Peru, Mercury.x, Mercury.y, 14, 14);
+            g.FillEllipse(Brushes.BurlyWood, Venus.x, Venus.y, 20, 20);
+            g.FillEllipse(Brushes.DodgerBlue, Earth.x, Earth.y, 24, 24);
+            g.FillEllipse(Brushes.Red, Mars.x, Mars.y, 14, 14);
+            g.FillEllipse(Brushes.Orange, Jupiter.x, Jupiter.y, 44, 44);
+            g.FillEllipse(Brushes.Khaki, Saturn.x, Saturn.y, 40, 40);
+            g.FillEllipse(Brushes.LightSkyBlue, Uranus.x, Uranus.y, 34, 34);
+            g.FillEllipse(Brushes.SteelBlue, Neptune.x, Neptune.y, 36, 36);
+            g.FillEllipse(Brushes.Brown, Pluto.x, Pluto.y, 12, 12);
+            MainPictureBox.Image = bitmp;
+        }
+
+        private void AnimationCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (AnimationCheckBox.Checked)
+            {
+                MercuryTimer.Enabled = true;
+                VenusTimer.Enabled = true;
+                EarthTimer.Enabled = true;
+                MarsTimer.Enabled = true;
+                JupiterTimer.Enabled = true;
+                SaturnTimer.Enabled = true;
+                UranusTimer.Enabled = true;
+                NeptuneTimer.Enabled = true;
+                PlutoTimer.Enabled = true;
+            }
+            else
+            {
+                MercuryTimer.Enabled = false;
+                VenusTimer.Enabled = false;
+                EarthTimer.Enabled = false;
+                MarsTimer.Enabled = false;
+                JupiterTimer.Enabled = false;
+                SaturnTimer.Enabled = false;
+                UranusTimer.Enabled = false;
+                NeptuneTimer.Enabled = false;
+                PlutoTimer.Enabled = false;
+            }
+        }
+
+        private void MercuryTimer_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.Black);
+
+            for (int i = 0; i < 500; i++)
+                DrawStars(bitmp, coordinateX[i], coordinateY[i]);
+
+            DrawPlanet(bitmp);
+
+            Mercury.Draw();
+        }
+
+        private void VenusTimer_Tick(object sender, EventArgs e)
+        {
+            Venus.Draw();
+        }
+
+        private void EarthTimer_Tick(object sender, EventArgs e)
+        {
+            Earth.Draw();
+        }
+
+        private void MarsTimer_Tick(object sender, EventArgs e)
+        {
+            Mars.Draw();
+        }
+
+        private void JupiterTimer_Tick(object sender, EventArgs e)
+        {
+            Jupiter.Draw();
+        }
+
+        private void SaturnTimer_Tick(object sender, EventArgs e)
+        {
+            Saturn.Draw();
+        }
+
+        private void UranusTimer_Tick(object sender, EventArgs e)
+        {
+            Uranus.Draw();
+        }
+
+        private void NeptuneTimer_Tick(object sender, EventArgs e)
+        {
+            Neptune.Draw();
+        }
+
+        private void PlutoTimer_Tick(object sender, EventArgs e)
+        {
+            Pluto.Draw();
+        }
+
+    }
+    public class Planet
+    {
+        public int r;  //радиус
+        public int x0;
+        public int y0;
+        public float x = 0, y = 0;
+        public double fi = 0.0;
+        public Planet(int rad, int x00, int y00) { r = rad; x0 = x00; y0 = y00; }
+        public void Draw()
+        {
+            fi += 0.01;
+            if (fi > 2 * Math.PI) fi = 0.0;
+            x = (float)(r * Math.Cos(fi) + x0);
+            y = (float)(r * Math.Sin(fi) + y0);
+        }
+    }
+}
